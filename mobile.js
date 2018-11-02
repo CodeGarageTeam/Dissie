44f37f531db87c964bd3da67b4de12a63418a56a
Ema Lorenzo <aemalorenzo@gmail.com>
2018-11-02 18:25:14 -0300

    fix adding item

--- mobile.js
+++ mobile.js
@@ -2,6 +2,6 @@ var button = document.getElementById('send-button')
 var buzon = document.getElementById('buzon')
 
 button.addEventListener('click', function () {
-  var mensage = document.createElement('LI')
+  var mensage = document.createElement('li')
   buzon.appendChild(mensage)
 })
