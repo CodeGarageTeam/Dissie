5797299141fab8f345412381fc86a7dedfb83136
Santiago Lorenzo <santylore4@gmail.com>
2018-11-02 19:00:52 -0300

    addMessage terminado

--- mobile.js
+++ mobile.js
@@ -1,7 +1,25 @@
 var button = document.getElementById('send-button')
-var buzon = document.getElementById('buzon')
+var input = document.getElementById('new-message-input')
 
-button.addEventListener('click', function () {
-  var mensage = document.createElement('li')
-  buzon.appendChild(mensage)
+button.addEventListener('click', addMessage)
+input.addEventListener('keypress', function (e) {
+  if (e.keyCode == 13) {
+    addMessage()
+  }
 })
+
+// crear nuevo mensage
+function addMessage() {
+  var buzon = document.getElementById('buzon')
+  var mensage = document.createElement('li')
+
+  // accediendo a lo q escribi
+  mensage.innerHTML = input.value
+
+  // reseteando el input
+  input.value = ''
+
+  // mando el mensage
+  buzon.appendChild(mensage)
+
+}
